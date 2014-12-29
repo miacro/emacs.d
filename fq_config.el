f9689b110c415538486386081d767946c1ed0429
guozhou <fqguozhou@163.com>
2014-12-29 18:12:22 +0800

    configuration for doxygen

---
+++ fq_config.el
@@ -0,0 +1,26 @@
+(global-linum-mode t)
+(setq inhibit-startup-buffer-menu t)
+(setq inhibit-startup-screen nil)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+;;(setq make-backup-files nil)
+(column-number-mode t)
+(show-paren-mode t)
+
+(display-time-mode 1)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+
+(mouse-avoidance-mode 'animate)
+;;(setq mouse-yank-at-point t)
+;;(transient-mark-mode t)
+
+(setq x-select-enable-clipboard t)
+(setq frame-title-format '("" buffer-file-name "@emacs" ))
+;;(setq default-fill-column 80) 
+
+(setq default-tab-width 4)
+(setq-default indent-tabs-mode nil)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(provide 'fq_config)
--- fq_elpa.el
+++ fq_elpa.el
@@ -1,11 +1,13 @@
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
-              '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
-              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (when (< emacs-major-version 24)
    ;; For important compatibility libraries like cl-lib
      (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
 
-(provide 'fq_melpa)
+(provide 'fq_elpa)
