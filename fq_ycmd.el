b047c8166b4f77c685da1f499ebab55305f5f574
fqaq <fqguozhou@gmail.com>
2014-12-30 20:49:24 +0800

    haha

--- fq_ycmd.el
+++ fq_ycmd.el
@@ -1,9 +1,9 @@
 (require 'ycmd)
 (ycmd-setup)
-(set-variable 'ycmd-server-command '("python2" "/home/think/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd"))
+(set-variable 'ycmd-server-command '("python" "/home/aq/.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd"))
 ;;(set-variable 'ycmd-server-command '("python2" "/home/think/github/emacs/ycmd/ycmd"))
-(set-variable 'ycmd-global-config "/home/think/.ycm_extra_conf.py")
-(set-variable 'ycmd-extra-conf-whitelist '("/home/think/cqthink/ehr_svc_v3/*"))
+(set-variable 'ycmd-global-config "/home/aq/.ycm_extra_conf.py")
+(set-variable 'ycmd-extra-conf-whitelist '("/home/aq/cqthink/ehr_svc_v3/*"))
 
 (require 'company-ycmd)
 (company-ycmd-setup)
