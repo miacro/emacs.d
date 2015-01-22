23b6f3c0351702276fac2f04f259b585ede0e1db
guozhou <fqguozhou@163.com>
2015-01-22 12:36:59 +0800

    change ecb configuration

--- lisp/fq-ecb.el
+++ lisp/fq-ecb.el
@@ -1,8 +1,21 @@
 ;;; fq-ecb --- Summary
 ;;; Commentary:
 ;;; Code
-
 ;;(require 'ecb-autoloads)
 (require 'ecb)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40")
+ '(ecb-tip-of-the-day nil)
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 (provide 'fq-ecb)
 ;;; fq-ecb ends here
