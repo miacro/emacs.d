368dca62c6c2f6a7ce43a9429c790db0a43a95de
guozhou <fqguozhou@163.com>
2015-02-14 15:15:30 +0800

    添加org-mode

---
+++ lisp/fq-org-mode.el
@@ -0,0 +1,15 @@
+;;; fq-org-mode --- Summary
+;;; Commentary:
+;;; Code:
+(add-to-list 'load-path "~/github/emacs/org-mode" t)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(transient-mark-mode 1)
+(setq truncate-lines nil)
+
+(require 'org-mode)
+(provide 'fq-org-mode)
+;;; fq-org-mode ends here
