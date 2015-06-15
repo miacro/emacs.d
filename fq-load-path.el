a0a9f76539a86be3954bb05e4d1d6176ba9258a5
miacro <fqguozhou@gmail.com>
2015-06-15 20:26:05 +0800

    haha

--- fq-load-path.el
+++ fq-load-path.el
@@ -5,8 +5,7 @@
 (add-to-list 'load-path "~/fq-emacs.d/lisp/program/company")
 (add-to-list 'load-path "~/fq-emacs.d/lisp/melpa")
 (add-to-list 'load-path "~/fq-emacs.d/lisp/el-get")
-(add-to-list 'load-path "~/fq-emacs.d/lisp/emacs")
-(add-to-list 'load-path "~/fq-emacs.d/lisp/version-control-system")
+(add-to-list 'load-path "~/fq-emacs.d/lisp/utils")
 (add-to-list 'load-path "~/fq-emacs.d/lisp/general-config")
 (provide 'fq-load-path)
 ;;; fq-load-path ends here
