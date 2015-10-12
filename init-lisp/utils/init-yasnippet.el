2e6cc5324d294f27805d1ba14da04540c9ee67f9
guozhou <fqguozhou@163.com>
2015-10-12 15:35:07 +0800

    yasnippet update

--- init-lisp/utils/init-yasnippet.el
+++ init-lisp/utils/init-yasnippet.el
@@ -2,6 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 (require 'yasnippet)
-(yas-global-mode 1)
+;; (yas-global-mode 1)
+(yas-reload-all)
+;;(add-hook 'irony-mode-hook #'yas-minor-mode)
 (provide 'init-yasnippet)
 ;;; init-yasnippet ends here
