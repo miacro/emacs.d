c8da97abf9d8d708d0a4432e26a4c61d1347b5c4
miacro <miacro@miacro.com>
2016-05-23 15:16:27 +0800

    update
    RT

--- init-lisp/utils/init-slime.el
+++ init-lisp/utils/init-slime.el
@@ -2,9 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 (require 'slime-autoloads)
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-;;(setq inferior-lisp-program "/usr/bin/sbcl")
+;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;;(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "/usr/bin/sbcl")
 (setq slime-contribs '(slime-fancy))
 (provide 'init-slime)
 ;;; init-slime ends here
