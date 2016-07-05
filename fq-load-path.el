f101d96aed34979d9921c715dcb9a72fd2dbafe8
miacro <fqguozhou@gmail.com>
2016-07-05 20:00:46 +0800

    update
    RT

--- fq-load-path.el
+++ fq-load-path.el
@@ -1,11 +1,11 @@
 ;;; fq-load-path --- Summary
 ;;; Commentary:
 ;;; Code
-(add-to-list 'load-path "~/fq-emacs.d/lisp")
-(add-to-list 'load-path "~/fq-emacs.d/init-lisp")
-(add-to-list 'load-path "~/fq-emacs.d/init-lisp/company")
-(add-to-list 'load-path "~/fq-emacs.d/init-lisp/melpa")
-(add-to-list 'load-path "~/fq-emacs.d/init-lisp/el-get")
-(add-to-list 'load-path "~/fq-emacs.d/init-lisp/utils")
+(add-to-list 'load-path "~/emacs.d/lisp")
+(add-to-list 'load-path "~/emacs.d/init-lisp")
+(add-to-list 'load-path "~/emacs.d/init-lisp/company")
+(add-to-list 'load-path "~/emacs.d/init-lisp/melpa")
+(add-to-list 'load-path "~/emacs.d/init-lisp/el-get")
+(add-to-list 'load-path "~/emacs.d/init-lisp/utils")
 (provide 'fq-load-path)
 ;;; fq-load-path ends here
