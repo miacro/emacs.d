04c089e3493c1d70bb5572fbe66deb7cf7a37f8a
miacro <fqguozhou@gmail.com>
2016-08-28 00:44:02 +0800

    ...

--- lisp/fonts.el
+++ lisp/fonts.el
@@ -3,5 +3,15 @@
 ;;; Code:
 (add-to-list 'default-frame-alist
  '(font . "Source Code Pro-13"))
+
+;; Setting English Font
+;;(set-face-attribute
+;;  'default nil :font "Source Code Pro 13")
+
+;; Chinese Font
+;;(dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;  (set-fontset-font (frame-parameter nil 'font)
+;;                    charset
+;;                    (font-spec :family "Microsoft YaHei" :size 12)))
 (provide 'fonts)
 ;;; fonts ends here
