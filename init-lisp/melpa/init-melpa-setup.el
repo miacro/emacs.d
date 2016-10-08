8661f66ceef936194b1ba8821c1798f663e68e26
miacro <miacro@miacro.com>
2016-10-08 19:08:41 +0800

    update package
    RT

--- init-lisp/melpa/init-melpa-setup.el
+++ init-lisp/melpa/init-melpa-setup.el
@@ -4,16 +4,12 @@
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives 
-             '("org" . "http://orgmode.org/elpa/") t)
+             '("orgmode" . "http://orgmode.org/elpa/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
