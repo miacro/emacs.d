012b9c383e1e8c51c34bffd24b166c50016fddd8
miacro <fqguozhou@gmail.com>
2017-02-07 12:19:17 +0800

    ....

--- lisp/theme.el
+++ lisp/theme.el
@@ -1,8 +1,10 @@
 ;;; theme --- Summary
 ;;; Commentary:
 ;;; Code:
-(require 'color-theme)
-(require 'color-theme-solarized)
-(color-theme-solarized-dark)
+(if (display-graphic-p)
+  (progn
+    (require 'color-theme)
+    (require 'color-theme-solarized)
+    (color-theme-solarized-dark)))
 (provide 'theme)
 ;;; theme ends here
