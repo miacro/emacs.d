7fc43a1f3aec44da0dc0f9e37b0a4a74124069bb
miacro <fqguozhou@gmail.com>
2017-02-10 21:40:28 +0800

    ...

--- init.el
+++ init.el
@@ -1,12 +1,16 @@
 ;;; init --- Summary
 ;;; Commentary:
 ;;; Code:
-(add-to-list 'load-path "~/.emacs.me/lisp")
-(add-to-list 'load-path "~/.emacs.me/init-lisp")
-(add-to-list 'load-path "~/.emacs.me/init-lisp/company")
-(add-to-list 'load-path "~/.emacs.me/init-lisp/melpa")
-(add-to-list 'load-path "~/.emacs.me/init-lisp/el-get")
-(add-to-list 'load-path "~/.emacs.me/init-lisp/utils")
+(defun add-to-load-path-based-on-current-lisp-file (subdir)
+  (add-to-list 'load-path
+   (concat 
+    (file-name-directory load-file-name) subdir)))
+(add-to-load-path-based-on-current-lisp-file "lisp")
+(add-to-load-path-based-on-current-lisp-file "init-lisp")
+(add-to-load-path-based-on-current-lisp-file "init-lisp/company")
+(add-to-load-path-based-on-current-lisp-file "init-lisp/melpa")
+(add-to-load-path-based-on-current-lisp-file "init-lisp/el-get")
+(add-to-load-path-based-on-current-lisp-file "init-lisp/utils")
 
 (require 'init-el-get)
 
