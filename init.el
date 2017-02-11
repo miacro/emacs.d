fee89a06bf8a927f9dbd93e283108e84c0660b1e
miacro <fqguozhou@gmail.com>
2017-02-11 20:35:52 +0800

    ..

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;; init --- Summary
 ;;; Commentary:
 ;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun add-to-load-path-based-on-current-lisp-file (subdir)
   (add-to-list 'load-path
    (concat 
