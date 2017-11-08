9c42062532ef396d36865a210ff5ff03dcc334f7
miacro <fqguozhou@gmail.com>
2017-11-08 15:36:26 +0800

    ...

--- init.el
+++ init.el
@@ -112,18 +112,3 @@
 
 (provide 'init)
 ;;; init ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (async yasnippet smex slime js2-mode goto-chg f emms color-theme-solarized))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
