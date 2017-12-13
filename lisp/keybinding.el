45ecb5d6fa439b3fd1a7408a8123315b4ce0e00b
miacro <fqguozhou@gmail.com>
2017-12-13 18:39:47 +0800

    ....

--- lisp/keybinding.el
+++ lisp/keybinding.el
@@ -3,5 +3,6 @@
 ;;; Code:
 (global-set-key (kbd "C-?") 'help-command)
 (global-set-key (kbd "C-m") 'set-mark-command)
+(global-unset-key (kbd "C-z"))
 (provide 'keybinding)
 ;;; keybinding ends here
