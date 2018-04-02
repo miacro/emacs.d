a11f171c444575f586555489a8cb36731c5c62f7
miacro <fqguozhou@gmail.com>
2018-04-02 11:23:21 +0800

    unbind keys of override-mode

--- lisp/keybinding.el
+++ lisp/keybinding.el
@@ -1,8 +1,14 @@
 ;;; keybinding --- Summary
 ;;; Commentary:
 ;;; Code:
+
+;; (define-key KEYMAP KEY nil)
+;; (global-unset-key KEY)
+;; (local-unset-key KEY)
 (global-set-key (kbd "C-?") 'help-command)
 (global-set-key (kbd "C-m") 'set-mark-command)
 (global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "<insertchar>"))
+(global-unset-key (kbd "<insert>"))
 (provide 'keybinding)
 ;;; keybinding ends here
