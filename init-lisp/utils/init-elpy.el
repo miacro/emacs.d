23f61f0b4b54e0eb9f8076067c044fe717c80ba6
miacro <fqguozhou@gmail.com>
2018-04-09 17:47:49 +0800

    update elpy config

--- init-lisp/utils/init-elpy.el
+++ init-lisp/utils/init-elpy.el
@@ -2,11 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-hook 'python-mode-hook
-          #'(lambda ()
-              (progn
-                (elpy-enable)
-                (elpy-use-ipython))))
-(setf elpy-rpc-timeout 15)
+(add-hook
+ 'python-mode-hook
+ #'(lambda ()
+     (progn
+       (elpy-enable)
+       (elpy-mode)
+       (setf elpy-rpc-timeout 15)
+       (cond
+        ((executable-find "jupyter")
+         (setq python-shell-interpreter "jupyter"
+               python-shell-interpreter-args
+               "console --simple-prompt"))
+        ((executable-find "ipython")
+         (setq python-shell-interpreter "ipython"
+               python-shell-interpreter-args
+               "-i --simple-prompt"))
+        ((executable-find "python")
+         (setq python-shell-interpreter "python"
+               python-shell-interpreter-args "-i"))))))
 (provide 'init-elpy)
 ;;; init-elpy ends here
