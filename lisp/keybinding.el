ca1df9fd40556b02e7f79191b8ef82f9bc66a986
miacro <fqguozhou@gmail.com>
2018-06-28 21:07:47 +0800

    ...

--- lisp/keybinding.el
+++ lisp/keybinding.el
@@ -5,13 +5,12 @@
 ;; (define-key KEYMAP KEY nil)
 ;; (global-unset-key KEY)
 ;; (local-unset-key KEY)
-(global-set-key (kbd "C-?") 'help-command)
+;; (global-set-key (kbd "C-?") 'help-command)
 (global-set-key (kbd "C-m") 'set-mark-command)
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "<insertchar>"))
 (global-unset-key (kbd "<insert>"))
 
-;;;; output of '$ stty -a' needed erase=^? (not ^H)
-;; (setq normal-erase-is-backspace t)
+;; (normal-erase-is-backspace-mode 0)
 (provide 'keybinding)
 ;;; keybinding ends here
