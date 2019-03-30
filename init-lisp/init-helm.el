706f207c5e052d0bc9245af9708ab3d1fd42ee4e
miacro <fqguozhou@gmail.com>
2019-03-30 16:12:14 +0800

    update init-packages to use use-package

--- init-lisp/init-helm.el
+++
@@ -1,9 +0,0 @@
-;;; init-helm --- Summary
-;;; Commentary:
-;;; Code:
-(require 'helm-config) ; Not needed if you use package.el
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(helm-mode 1)
-(provide 'init-helm)
-;;; init-helm ends here
--- init-lisp/init-packages.el
+++ init-lisp/init-packages.el
@@ -5,9 +5,12 @@
   (el-get-bundle use-package)
 
   (el-get-bundle helm)
-  (require 'init-helm)
-
-  (el-get-bundle 2048.el)
+  (use-package helm
+    :requires helm-config ; Not needed if you use package.el
+    :config
+    (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
+    (helm-mode 1))
 
   (el-get-bundle color-theme)
   (el-get-bundle color-theme-solarized)
@@ -18,91 +21,98 @@
     (when (display-graphic-p) nil))
 
   (el-get-bundle org-mode)
-  (require 'init-org-mode)
+  (use-package init-org-mode)
   (el-get-bundle emms)
-  (el-get-bundle js2-mode)
-  (el-get-bundle s)
-  (el-get-bundle f)
-  (el-get-bundle let-alist)
 
   (el-get-bundle slime)
-  (require 'init-slime)
+  (use-package init-slime)
   (el-get-bundle yasnippet)
-  (require 'init-yasnippet)
+  (use-package init-yasnippet)
   ;;(el-get-bundle auto-complete)
   ;;(el-get-bundle auto-complete-clang)
   ;;(el-get-bundle clang-complete-async)
 
   (el-get-bundle emacs-w3m)
-  (require 'init-emacs-w3m)
+  (use-package init-emacs-w3m)
   ;;(el-get-bundle ecb)
-  ;;(require 'init-ecb)
+  ;;(use-package init-ecb)
   ;;(el-get-bundle smex)
-  ;;(require 'init-smex)
+  ;;(use-package init-smex)
 
   ;;(el-get-bundle elpa:undo-tree)
   (el-get-bundle evil)
-  (require 'init-evil)
+  (use-package init-evil)
 
   (el-get-bundle ghub)
   (el-get-bundle magit)
 
-  (require 'init-magit)
+  (use-package init-magit)
   ;; (el-get-bundle doxymacs)
-  ;; (require 'init-doxymacs)
+  ;; (use-package init-doxymacs)
   (el-get-bundle flycheck)
-  (require 'init-flycheck)
+  (use-package init-flycheck)
   (el-get-bundle google-c-style)
-  (require 'init-google-c-style)
+  (use-package init-google-c-style)
   (el-get-bundle slim-mode)
-  (require 'init-slim-mode)
+  (use-package init-slim-mode)
 
   (el-get-bundle company-mode)
-  (require 'init-company-mode)
+  (use-package init-company-mode)
   (el-get-bundle slime-company)
-  (require 'init-slime-company)
+  (use-package init-slime-company)
   ;;(el-get-bundle company-cmake)
-  ;;(require 'init-company-cmake)
+  ;;(use-package init-company-cmake)
   (el-get-bundle company-c-headers)
-  (require 'init-company-c-headers)
+  (use-package init-company-c-headers)
   (el-get-bundle highlight-symbol)
-  (require 'init-highlight-symbol)
+  (use-package init-highlight-symbol)
   ;;;; ycmd completion
   ;;(el-get-bundle emacs-ycmd)
-  ;;(require 'init-emacs-ycmd)
+  ;;(use-package init-emacs-ycmd)
 
   ;; irony-mode
   ;;(el-get-bundle irony-mode)
-  ;;(require 'init-irony-mode)
+  ;;(use-package init-irony-mode)
   ;;(el-get-bundle company-irony)
-  ;;(require 'init-company-irony)
+  ;;(use-package init-company-irony)
   ;;(el-get-bundle flycheck-irony)
-  ;;(require 'init-flycheck-irony)
+  ;;(use-package init-flycheck-irony)
   ;;(el-get-bundle irony-eldoc)
-  ;;(require 'init-irony-eldoc)
+  ;;(use-package init-irony-eldoc)
 
   ;; rtags
   ;;(el-get-bundle rtags)
 
   (el-get-bundle clang-format)
-  (require 'init-clang-format)
+  (use-package init-clang-format)
   (el-get-bundle e2wm)
-  (require 'init-e2wm)
+  (use-package init-e2wm)
   (el-get-bundle bison-mode)
-  (require 'init-bison-mode)
+  (use-package init-bison-mode)
 
-  ;;(el-get-bundle srefactor)
-  ;;(require 'init-srefactor)
   (el-get-bundle ace-jump-mode)
   (el-get-bundle graphviz-dot-mode)
 
-  (when (not (string= (el-get-read-package-status "elpy") "installed"))
-    (message
-      (shell-command-to-string
-        "pip install --user jedi flake8 importmagic autopep8 yapf")))
+  ;; (when (not (string= (el-get-read-package-status "elpy") "installed"))
+  ;;   (message
+  ;;     (shell-command-to-string
+  ;;       "pip install --user jedi flake8 importmagic autopep8 yapf")))
   (el-get-bundle elpy)
-  (require 'init-elpy)
-  ;;(el-get-bundle skewer-mode)
+  (use-package init-elpy
+    :ensure-system-package
+    ((flake8 . "pip install --user jedi flake8")
+     (autopep8 . "pip install --user autopep8")
+     (yapf . "pip install --user yapf")))
+
+  (el-get-bundle skewer-mode)
+  (use-package skewer-mode
+    :init
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+    :hook
+    (js2-mode-hook . skewer-mode)
+    (css-mode-hook . skewer-css-mode)
+    (html-mode-hook . skewer-html-mode))
+  (el-get-bundle rjsx-mode)
   ;;(el-get-bundle ein)
 
   (el-get-bundle yaml-mode)
@@ -110,14 +120,14 @@
   (el-get-bundle dockerfile-mode)
 
   (el-get-bundle protobuf-mode)
-  (require 'init-protobuf-mode)
-  (require 'dockerfile-mode)
+  (use-package init-protobuf-mode)
+  (use-package dockerfile-mode)
 
   (el-get-bundle plantuml-mode)
   (use-package plantuml-mode
-    :config 
+    :config
     (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode)))
-  
+
   (package-install 'org-mind-map)
   ;; This is an Emacs package that creates graphviz directed graphs from
   ;; the headings of an org file
