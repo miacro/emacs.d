9d9ad13716c57abb886bf573758c9ca184b9a8e1
miacro <fqguozhou@gmail.com>
2019-04-27 11:29:42 +0800

    ...

--- init-lisp/init-misc-company.el
+++ init-lisp/init-misc-company.el
@@ -6,6 +6,7 @@
   :hook
   (after-init-hook . global-company-mode)
   :init
+  (add-hook 'after-init-hook 'global-company-mode)
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 2))
 
