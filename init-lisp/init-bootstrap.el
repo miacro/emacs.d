6e16b2e5b11f65b534b0a4d7af873c5788479899
miacro <fqguozhou@gmail.com>
2019-04-27 12:09:59 +0800

    update color-theme

--- init-lisp/init-bootstrap.el
+++ init-lisp/init-bootstrap.el
@@ -23,11 +23,17 @@
   (el-get-bundle color-theme-solarized)
   (use-package color-theme-solarized
     :requires color-theme
-    :config
+    :init
     (load-theme 'solarized t)
+    (add-hook 'after-make-frame-functions
+       (lambda (frame)
+         (let ((mode (if (display-graphic-p frame) 'dark 'dark)))
+           (set-frame-parameter frame 'background-mode mode)
+           (set-terminal-parameter frame 'background-mode mode))
+         (enable-theme 'solarized)))
     (set-frame-parameter nil 'background-mode 'dark)
-    (enable-theme 'solarized)
-    (when (display-graphic-p) nil))
+    (set-terminal-parameter nil 'background-mode 'dark)
+    (enable-theme 'solarized))
 
   (el-get-bundle emms)
 
