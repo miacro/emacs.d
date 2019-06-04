d67a15cc2262f655a3bd9fa4901513a9a8f81e90
miacro <fqguozhou@gmail.com>
2019-06-04 17:10:00 +0800

    ...

--- init-lisp/init-misc-python.el
+++ init-lisp/init-misc-python.el
@@ -10,7 +10,8 @@
 (use-package elpy
   :ensure-system-package
   ((flake8 . "pip install --user jedi flake8")
-   (autopep8 . "pip install --user autopep8")
+   ;;(autopep8 . "pip install --user autopep8")
+   ;;(black . "pip install --user black")
    (yapf . "pip install --user yapf"))
   ;; :hook
   ;; (python-mode-hook . elpy-mode-enable-test)
